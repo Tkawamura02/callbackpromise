f58600d35e4347b6678735ebc6fbf4033e3ac6f1
Tiffany Kawamura <86638759+Tkawamura02@users.noreply.github.com>
2022-09-03 21:52:42 -0700

    Final Draft
    edited the promises file

--- promises.js
+++ promises.js
@@ -27,4 +27,14 @@ function createPost(post) {
     });
 }
 
-createPost({title: 'Post Three', body: 'This is the third post'});
+createPost({title: 'Post Three', body: 'This is the third post'})
+    .then(getPosts) //once loop resolves then.... getPosts
+    .catch(err => console.log(err)); //catching the error
+
+
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res=>res.json());
+
+Promise.all([promise1, promise2, promise3, promise4]).then((values)=>console.log(values));
